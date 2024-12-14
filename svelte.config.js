f21b1e102e209ef15b9bb1013d650e56227b7664
kiloooooooo <kilotango37@icloud.com>
2024-12-14 18:06:06 +0900

    GitHub Pagesで公開する準備

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,8 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://svelte.dev/docs/kit/integrations
@@ -11,15 +13,10 @@ const config = {
         // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
         // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
         // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-        adapter: adapter({
-            pages: 'build',
-            assets: 'build',
-            precompress: false,
-            strict: true
-        }),
+        adapter: adapter(),
         paths: {
-            base: '',
-            relative: true
+            base: dev ? '' : '/gas-preprocess-visualizer',
+            // relative: true
         }
     }
 };
